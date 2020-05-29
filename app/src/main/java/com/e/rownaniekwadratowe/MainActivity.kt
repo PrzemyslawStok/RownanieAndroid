e8e8c42545d995d3daff60e5f76587b70d1171a8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-05-29 15:08:30 +0200

    cd..

--- app/src/main/java/com/e/rownaniekwadratowe/MainActivity.kt
+++ app/src/main/java/com/e/rownaniekwadratowe/MainActivity.kt
@@ -3,6 +3,8 @@ package com.e.rownaniekwadratowe
 import RownanieKwadratowe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,14 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        oblicz.setOnClickListener {
-            val rownanieKwadratowe = RownanieKwadratowe(A_text.text.toString().toDouble(),B_text.text.toString().toDouble(),C_text.text.toString().toDouble())
-
-            rownanieText.setText(rownanieKwadratowe.pobierzRownanie())
-
-            rownanieKwadratowe.wyswietlRownanie()
-            rownanieKwadratowe.wyswietlRozwiazania()
+        button1.setOnClickListener {
+            val A_string = A_text.text.toString()git
+            Log.v("przykładowy tag","Pole tekstowe: "+A_string)
         }
 
+        //val rownanieKwadratowe = RownanieKwadratowe(A_text.text.toString().toDouble(),B_text.text.toString().toDouble(),C_text.text.toString().toDouble())
     }
 }
